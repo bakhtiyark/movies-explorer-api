a08ece694fd3b070e04b552dfb4e2df18c97ae03
Bakhtiyar K <bakhtiyark234@gmail.com>
2022-11-01 18:37:29 +0500

    Extra

--- routes/movies.js
+++ routes/movies.js
@@ -3,43 +3,36 @@ const { celebrate, Joi } = require('celebrate');
 const { regexpLink } = require('../utils/constants');
 
 const {
-  createCard,
-  getCards,
-  deleteCard,
-  likeCard,
-  removeLike,
-} = require('../controllers/cards');
+  createMovie,
+  getMovies,
+  deleteMovie,
+} = require('../controllers/movies');
 
 // Получение данных всех карточек
-router.get('/', getCards);
+router.get('/', getMovies);
 
 // Создание карточки
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    link: Joi.string().required(true).pattern(regexpLink),
+    country: Joi.string().min(2).max(30).required(),
+    director: Joi.string().min(2).max(30).required(),
+    duration: Joi.number().required(),
+    year: Joi.number().required(),
+    description: Joi.string().min(2).max(30).required(),
+    image: Joi.string().required(true).pattern(regexpLink),
+    trailer: Joi.string().required(true).pattern(regexpLink),
+    nameRU: Joi.string().min(2).max(30).required(),
+    nameEN: Joi.string().min(2).max(30).required(),
+    thumbnail: Joi.string().required(true).pattern(regexpLink),
+    movieId: Joi.string().hex().required(true)
   }),
-}), createCard);
+}), createMovie);
 
-// Удаление карточки
-router.delete('/:cardId', celebrate({
+// Удаление фильма
+router.delete('/:movieId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex().required(),
+    movieId: Joi.string().length(24).hex().required(),
   }),
-}), deleteCard);
-
-// Лайканье карточки
-router.put('/:cardId/likes', celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex().required(),
-  }),
-}), likeCard);
-
-// Снятие лайка с карточки
-router.delete('/:cardId/likes', celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex().required(),
-  }),
-}), removeLike);
+}), deleteMovie);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,13 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const { regexpLink } = require('../utils/constants');
 
 const {
-  getUsers,
-  getUser,
   getCurrentUser,
   patchUser,
-  patchAvatar,
 } = require('../controllers/users');
-
-// Получение данных всех пользователей
-router.get('/', getUsers);
 
 // Получение данных текущего
 router.get('/me', getCurrentUser);
-
-// Получение данных конкретного пользователя по ID
-router.get('/:userId', celebrate({
-  params: Joi.object().keys({
-    userId: Joi.string().length(24).hex().required(),
-  }),
-}), getUser);
 
 // Обновление данных пользователя
 router.patch('/me', celebrate({
@@ -30,12 +16,5 @@ router.patch('/me', celebrate({
     about: Joi.string().min(2).max(30),
   }),
 }), patchUser);
-
-// Замена аватара
-router.patch('/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().required().pattern(regexpLink),
-  }),
-}), patchAvatar);
 
 module.exports = router;
